881b99b414b6690f9d65b407056c9099dc67d647
jkwong888 <jkwong@ca.ibm.com>
2017-07-17 17:39:56 +0800

    base64 decode secrets before using them

--- docker/startup.sh
+++ docker/startup.sh
@@ -19,12 +19,22 @@ export JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom"
 source ./agents/newrelic.sh
 
 # open the secrets
-hs256_key=`cat /var/run/secrets/hs256-key/key`
-cloudant_username=`cat /var/run/secrets/binding-refarch-cloudantdb/binding | jq '.username' | sed -e 's/"//g'`
-cloudant_password=`cat /var/run/secrets/binding-refarch-cloudantdb/binding | jq '.password' | sed -e 's/"//g'`
-cloudant_host=`cat /var/run/secrets/binding-refarch-cloudantdb/binding | jq '.host' | sed -e 's/"//g'`
-cloudant_port=`cat /var/run/secrets/binding-refarch-cloudantdb/binding | jq '.port' | sed -e 's/"//g'`
-cloudant_proto=`cat /var/run/secrets/binding-refarch-cloudantdb/binding | jq '.url' | sed -e 's/"//g' | sed -e 's|://.*||'`
+if [ ! -z "${HS256_KEY}" ]; then
+  hs256_key=${HS256_KEY}
+else
+  hs256_key=`cat /var/run/secrets/hs256-key/key`
+fi
+
+if [ -z "${couchdb}" ]; then
+    echo "Error: couchdb is not defined in environment!"
+    exit 1
+fi
+
+cloudant_username=`echo ${couchdb} | base64 -d | jq -r '.username'`
+cloudant_password=`echo ${couchdb} | base64 -d | jq -r '.password'`
+cloudant_host=`echo ${couchdb} | base64 -d | jq -r  '.host'`
+cloudant_port=`echo ${couchdb} | base64 -d | jq -r '.port'`
+cloudant_proto=`echo ${couchdb} | base64 -d | jq -r '.url' | sed -e 's|://.*||'`
 
 JAVA_OPTS="${JAVA_OPTS} -Dspring.application.cloudant.username=${cloudant_username} -Dspring.application.cloudant.password=${cloudant_password} -Dspring.application.cloudant.host=${cloudant_host} -Dspring.application.cloudant.port=${cloudant_port} -Dspring.application.cloudant.protocol=${cloudant_proto}"
 JAVA_OPTS="${JAVA_OPTS} -Djwt.sharedSecret=${hs256_key}"
