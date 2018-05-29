93f6b85a9c6437aa57df2ec5f8990f397f5dfda4
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2018-05-29 16:09:48 -0500

    removing unused imports

--- src/main/java/application/rest/client/CloudantClientService.java
+++ src/main/java/application/rest/client/CloudantClientService.java
@@ -1,28 +1,14 @@
 package application.rest.client;
 
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.UriBuilder;
-import javax.ws.rs.core.UriInfo;
 
-import java.util.Properties;
 import javax.enterprise.context.Dependent;
-import javax.ws.rs.ProcessingException;
 import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import javax.enterprise.context.RequestScoped;
 
 @Dependent
 @RegisterRestClient
